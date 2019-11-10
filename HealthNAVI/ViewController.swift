3f6690ed17c2a41c9fcc703fb2f6c1cf37708999
arkray - Francis Bergonia <fbergonia.arkray@gmail.com>
2019-11-11 07:47:36 +0800

    Initial Commit

---
+++ HealthNAVI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthNAVI
+//
+//  Created by Francis Jemuel Bergonia on 11/11/19.
+//  Copyright © 2019 Arkray Marketing, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
